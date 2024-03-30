21f24a64a861a1e299f0ea4db1ed57b907704741
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-03-30 19:37:59 +0530

    added buttons and functionality

--- AI-Chatapp/AI_Chatapp/AI_Chatapp.py
+++ AI-Chatapp/AI_Chatapp/AI_Chatapp.py
@@ -1,45 +1,60 @@
 from rxconfig import config
 import reflex as rx
 from AI_Chatapp import style
+from AI_Chatapp.state import State
 
-def qa(question:str, answer:str) -> rx.Component:
+def qa(question: str, answer: str) -> rx.Component:
     return rx.box(
-        rx.box(question, text_align="centre", style=style.question_style),
-        rx.box(answer, text_align="centre", style=style.answer_style),
+        rx.box(
+            rx.text(question, style=style.question_style),
+            text_align="right",
+        ),
+        rx.box(
+            rx.text(answer, style=style.answer_style),
+            text_align="left",
+        ),
         margin_y="1em",
     )
 
+
 def chat() -> rx.Component:
-    qa_pairs = [
-        (
-            "What is Reflex?",
-            "A way to build web apps in pure Python!",
-        ),
-        (
-            "What can I make with it?",
-            "Anything from a simple website to a complex web app!",
-        ),
-    ]
     return rx.box(
-        *[
-            qa(question, answer)
-            for question, answer in qa_pairs
-        ]
+        rx.foreach(
+            State.chat_history,
+            lambda messages: qa(messages[0], messages[1])
+        )
     )
 
-def question_bar() -> rx.Component:
+
+def action_bar() -> rx.Component:
     return rx.hstack(
-        rx.input(placeholder="Ask me a question : "),
-        rx.button("ASK"),
+        rx.input(
+            value=State.question,
+            placeholder="Ask a question",
+            on_change=State.set_question,
+            style=style.input_style,
+        ),
+        rx.button(
+            "Ask", 
+            on_click=State.answer,
+            style=style.button_style,
+        ),
+        rx.button(
+            "Clear Chat",
+            on_click=State.clear_chat,
+            style=style.button_style,
+        )
     )
 
 
 def index() -> rx.Component:
-    return rx.container(
-        chat(),
-        question_bar(),
+    return rx.center(
+        rx.vstack(
+            chat(),
+            action_bar(),
+            align="center",
         )
-
+    )
 
 
 # def index() -> rx.Component:
--- AI-Chatapp/AI_Chatapp/style.py
+++ AI-Chatapp/AI_Chatapp/style.py
@@ -1,6 +1,7 @@
 # style.py
 import reflex as rx
 
+
 # Common styles for questions and answers.
 shadow = "rgba(0, 0, 0, 0.15) 0px 2px 8px"
 chat_margin = "20%"
