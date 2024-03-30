cc8796e506d335ee565f8698326c4a5160dbc6d4
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-03-31 00:25:02 +0530

    added nice streaming effect:

--- AI-Chatapp/AI_Chatapp/state.py
+++ AI-Chatapp/AI_Chatapp/state.py
@@ -1,4 +1,5 @@
 import reflex as rx
+import asyncio
 
 
 class State(rx.State):
@@ -8,11 +9,22 @@ class State(rx.State):
     # Keep track of the chat history as a list of (question, answer) tuples.
     chat_history: list[tuple[str, str]]
 
-    def answer(self):
+    async def answer(self):
         # Our chatbot is not very smart right now...
         answer = "I don't know!"
-        self.chat_history.append((self.question, answer))
+        self.chat_history.append((self.question, ""))
         self.question=''
+        yield
+        
+        for i in range(len(answer)):
+            # Pause to show the streaming effect
+            await asyncio.sleep(0.1)
+            # Adding one letter at a time
+            self.chat_history[-1] = (
+                self.chat_history[-1][0],
+                answer[: i + 2],
+            )
+            yield
     
     def clear_chat(self):
         self.chat_history = []
