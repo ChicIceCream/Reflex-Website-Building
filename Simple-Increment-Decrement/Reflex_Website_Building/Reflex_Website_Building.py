d154869500ba9a8a9115b9f4cadad9c868692bd8
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-05-12 04:45:39 +0530

    Fixed

--- Simple-Increment-Decrement/Reflex_Website_Building/Reflex_Website_Building.py
+++ Simple-Increment-Decrement/Reflex_Website_Building/Reflex_Website_Building.py
@@ -10,25 +10,45 @@ class State(rx.State):
     def decrement(self):
         self.count -= 1
 
+# class CondSimpleState(rx.State):
+#     show: bool = True
+
+#     def change(self):
+#         self.show = not (self.show)
+
+
+# def cond_simple_example():
+#     return rx.vstack(
+#         rx.button(
+#             "Toggle", on_click=CondSimpleState.change
+#         ),
+#         rx.cond(
+#             CondSimpleState.show,
+#             rx.text("Text 1", color="blue"),
+#             rx.text("Text 2", color="red"),
+#         ),
+#     )
 
 def index() -> rx.Component:
         return rx.container(
-            rx.color_mode.button("Change"),
-            rx.hstack(
-                rx.button(
-                    "Decrement",
-                    color_scheme="ruby",
-                    on_click=State.decrement,
-                ),
-                rx.heading(State.count, font_size="2em"),
-                rx.button(
-                    "Increment",
-                    color_scheme="grass",
-                    on_click=State.increment,
-                ),
-            spacing="3",
-        ),
+            rx.center(
+                # rx.color_mode.button("Change"),
+                rx.hstack(
+                    rx.button(
+                        "Decrement",
+                        color_scheme="ruby",
+                        on_click=State.decrement,
+                    ),
+                    rx.heading(State.count, font_size="2em"),
+                    rx.button(
+                        "Increment",
+                        color_scheme="grass",
+                        on_click=State.increment,
+                    ),
+                spacing="3",
+            ),
+            # cond_simple_example(),
+        )
     )
-
 app = rx.App()
 app.add_page(index)
