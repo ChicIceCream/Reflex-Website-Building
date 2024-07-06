499930f2463be34a34c830f5b5ff73206da1f8f4
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-07-06 23:16:35 +0530

    changes

--- full-stack-python/full_stack_python/pages/blog1.py
+++ full-stack-python/full_stack_python/pages/blog1.py
@@ -7,10 +7,16 @@ def blog1_page() -> rx.Component:
         rx.vstack(
             rx.heading("What is Supervised Learning?", size="9"),
             rx.text(
-                "Supervised learning is a type of machine learning where the model is trained on a labeled dataset. ",
+                """
+                Supervised learning is a type of machine learning 
+                where the model is trained on a labeled dataset. 
+                """,
             ),
             rx.code(
-                "import sklearn as sk\n",
+                """
+                import sklearn as sk\n 
+                import the world!\n
+                """,
                 lang="python",
             ),
             spacing="5",
